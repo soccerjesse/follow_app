73926205f0395a7094389393ca076e54d6d33e15
soccerjesse <jishperu10@gmail.com>
2022-04-15 19:52:15 +0900

    finish follow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_080224) do
+ActiveRecord::Schema.define(version: 2022_04_06_081823) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
